c84e133d1a75a854fd78e69be941cf9907ff5b12
Alexander Mandrikov <mandrikov@pm.me>
2022-08-21 13:26:19 +0700

    add level in configuration

--- lib/named_logger/configuration.rb
+++ lib/named_logger/configuration.rb
@@ -6,8 +6,10 @@ module NamedLogger
   class Configuration
     include Singleton
 
-    attr_accessor :disabled, :formatter, :console_max_message_size, :console_proxy,
-                  :dirname, :filename, :severity, :environment
+    attr_accessor :disabled, :formatter, :console_max_message_size,
+                  :console_proxy, :dirname, :filename
+
+    attr_reader :level
 
     def initialize
       set_defaults
@@ -16,8 +18,7 @@ module NamedLogger
     def set_defaults
       self.disabled = false
 
-      self.severity = :debug #! check docs
-      self.environment = nil
+      self.level = :debug
       self.formatter = Formatter.new
       self.dirname = File.join(__dir__, 'log', 'named_logger')
       self.filename = ->(logger_name, env = nil) { [logger_name, env, 'log'].compact.join('.') }
@@ -26,6 +27,18 @@ module NamedLogger
       self.console_max_message_size = 512
 
       true
+    end
+
+    def level=(severity)
+      @level =
+        case severity.to_s.downcase
+        when 'debug'  then ::Logger::DEBUG
+        when 'info'   then ::Logger::INFO
+        when 'warn'   then ::Logger::WARN
+        when 'error'  then ::Logger::ERROR
+        when 'fatal'  then ::Logger::FATAL
+        else ::Logger::UNKNOWN
+        end
     end
   end
 end
