ba0c00707a78be9c2190df381bcfad884adfff3d
Alexander Mandrikov <mandrikov@pm.me>
2022-08-21 13:27:49 +0700

    add comment for formatter datetime format

--- lib/named_logger/formatter.rb
+++ lib/named_logger/formatter.rb
@@ -1,10 +1,14 @@
 # frozen-string-literal: true
 
+require 'logger'
+
 module NamedLogger
   class Formatter < ::Logger::Formatter
     def initialize
       super
 
+      # TODO replace format to DatetimeFormat constant and delete last space
+      # https://github.com/ruby/ruby/commit/a8b11b5cdd5fedd30a65e60bdae4c00d259d4191
       @datetime_format = '%Y-%m-%d %H:%M:%S '
     end
   end
