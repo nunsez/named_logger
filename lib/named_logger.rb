addb568069c4862564401ba245ae2030de57acfd
Alexander Mandrikov <mandrikov@pm.me>
2022-08-21 13:28:55 +0700

    extend version into main module

--- lib/named_logger.rb
+++ lib/named_logger.rb
@@ -6,10 +6,10 @@ require 'delegate'
 require 'fileutils'
 require 'logger'
 require 'singleton'
-
 require_relative 'named_logger/version'
 require_relative 'named_logger/logger'
 
 module NamedLogger
+  extend Version
   extend Logger
 end
