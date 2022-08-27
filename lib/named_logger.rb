978a204706db5a4f709fd2fd775174b3d32a3d84
Alexander Mandrikov <mandrikov@pm.me>
2022-08-27 22:26:04 +0700

    remove version module

--- lib/named_logger.rb
+++ lib/named_logger.rb
@@ -4,6 +4,5 @@ require_relative 'named_logger/version'
 require_relative 'named_logger/logger'
 
 module NamedLogger
-  extend Version
   extend Logger
 end
--- lib/named_logger/version.rb
+++ lib/named_logger/version.rb
@@ -1,15 +1,9 @@
 # frozen-string-literal: true
 
 module NamedLogger
-  module Version
-    MAJOR = 0
-    MINOR = 1
-    PATCH = 0
+  MAJOR = 0
+  MINOR = 1
+  PATCH = 0
 
-    VERSION = [MAJOR, MINOR, PATCH].join('.').freeze
-
-    def self.extended(othermod)
-      othermod.const_set(:VERSION, VERSION)
-    end
-  end
+  VERSION = [MAJOR, MINOR, PATCH].join('.')
 end
