3a2199afb417c9e6cecab20ef816c9673d16e209
Alexander Mandrikov <mandrikov@pm.me>
2022-09-05 15:03:35 +0700

    refactor console proxy tests

--- test/lib/console_proxy_test.rb
+++ test/lib/console_proxy_test.rb
@@ -5,29 +5,38 @@ require 'test_helper'
 
 class ConsoleProxyTest < Minitest::Test
   def test_stdout
-    # config = NamedLogger::Configuration.new
-    # config.disabled = true
-    # logger = NamedLogger::LoggerBuilder.new(nil).build(config)
-    logger = Logger.new(nil)
-    proxy = NamedLogger::ConsoleProxy.new(logger)
-
-    # proxy.debug { Hash.new }
+    config = NamedLogger::Configuration.new(disabled: true, console_proxy: true)
+    logger = NamedLogger::LoggerBuilder.new(nil, config: config)
 
     assert_output("foo\n") do
-      proxy.debug { 'foo' }
+      logger.debug { 'foo' }
     end
 
-    assert_output("foo: bar\n") do
-      proxy.info('foo') { 'bar' }
+    assert_output("foo: {}\n") do
+      logger.info('foo') { Hash.new }
+    end
+  end
+
+  def test_stdout_length
+    length = 100
+    config = NamedLogger::Configuration.new(disabled: true,
+                                            console_proxy: true,
+                                            console_max_message_size: length)
+    logger = NamedLogger::LoggerBuilder.new(nil, config: config)
+    obj = Class.new do
+      def initialize(len)
+        @foo = 'bar ' * len
+      end
+    end.new(length)
+
+    assert_output(/.{,#{length}}/) do
+      logger.info(obj)
     end
   end
 
   def test_stdout_when_disabled
-    config = NamedLogger::Configuration.new
-    config.disabled = true
-
-    builder = NamedLogger::LoggerBuilder.new(nil)
-    logger = builder.build(config)
+    config = NamedLogger::Configuration.new(disabled: true, console_proxy: false)
+    logger = NamedLogger::LoggerBuilder.new(nil, config: config)
 
     assert_silent { logger.info('foo') }
   end
